ffb25730a08dd620e45b67646a6b443a393f0838
finsherwell <finleysherwell@icloud.com>
2025-07-15 15:39:41 +0100

    Sync src/ and include/ from dev

--- include/constants.h
+++ include/constants.h
@@ -6,4 +6,5 @@
 namespace Constants {
     constexpr size_t CACHE_LINE_SIZE = 64;
     constexpr size_t PAGE_SIZE = 4096;
+    constexpr size_t STANDARD_ALIGNMENT = 8;
 }
---
+++ include/thread_local/thread_local_allocator.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstddef>
+#include <atomic>
+
+#include "constants.h"
+#include "utils.h"
+
+template <size_t Size>
+class ThreadLocalAllocator
+{
+private:
+    alignas(Constants::CACHE_LINE_SIZE) thread_local static std::byte buffer_[Size];
+    alignas(Constants::CACHE_LINE_SIZE) thread_local static size_t offset_;
+public:
+    void* allocate(size_t size, size_t alignment = alignment(std::max_align_t)) {
+        size_t aligned_size = Utils::Memory::align_up<Constants::STANDARD_ALIGNMENT>(size);
+
+        size_t aligned_offset = Utils::Memory::align_up<Constants::STANDARD_ALIGNMENT>(offset_);
+        size_t new_offset = aligned_offset + aligned_size;
+
+        if (new_offset > size) {
+            return nullptr;
+        }
+
+        offset_ = new_offset;
+        return buffer_ + aligned_offset;
+    }
+
+    void deallocate() {
+        // No-op for linear allocator
+    }
+
+    void reset() {
+        offset_ = 0;
+    }
+
+    bool owns(void* ptr) const {
+        return ptr >= buffer_ && ptr < buffer_ + Size;
+    }
+
+    size_t used() const { return offset_; }
+    constexpr size_t capacity const { return Size; }
+};
+
+// Thread-local static definitions
+template <size_t Size>
+alignas(Constants::CACHE_LINE_SIZE) thread_local std::byte ThreadLocalAllocator<Size>::buffer_[Size];
+
+template <size_t Size>
+thread_local size_t ThreadLocalAllocator<Size>::offset_ = 0;
